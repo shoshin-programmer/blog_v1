7324dd4a23cb324d33274addb80c53aad43d501b
arvin <61683592+markryu14@users.noreply.github.com>
2020-06-28 13:54:22 +0800

    added models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from api.models import Post, Tag
 
 # Register your models here.
+
+admin.site.register(Tag)
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
+    list_display = ('title', 'list_tags')
+
+
+admin.site.register(Post, PostAdmin)
