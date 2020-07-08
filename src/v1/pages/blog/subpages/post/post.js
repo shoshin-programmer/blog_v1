cf25af718e07fa253ea70d3e062b1eeea17be86d
arvin <61683592+markryu14@users.noreply.github.com>
2020-07-08 22:34:03 +0800

    individual post consume api

--- src/v1/pages/blog/subpages/post/post.js
+++ src/v1/pages/blog/subpages/post/post.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import BlogLoading from "../../../../components/loading/loading";
+import axios from "axios";
 
 export default function IndividualPost(props) {
-    let { id } = useParams();
+  const [post, setPost] = useState({
+    title: "Post Title",
+    post_img: "",
+    content: "",
+    tag: []
+  });
+  let { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios(`http://127.0.0.1:8000/api/post/${id}/`);
+      setPost(response.data);
+      console.log(response.data);
+    };
+    fetchData();
+  }, [id]);
+
   return (
     <React.Fragment>
       <BlogLoading />
       <div className="blog-instance">
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            height: "90vh",
-            color: "#8892b0"
-          }}
-        >
-          <h1>word{id}</h1>
+        <div className="recent-header-wrap">
+          <div className="recent-title">{post.title}</div>
+        </div>
+        <div className="post-container">
+          <div className="recent-post-wrap">
+            <div className="post-img-wrap">
+              <img src={post.post_img} className="post-img" alt={post.title} />
+            </div>
+            <div className="each-recent-post">
+              <div className="post-category">
+                {post.tag.map((tag, index) => (
+                  <span key={index}>{tag.name} </span>
+                ))}
+              </div>
+              <div className="post-text">{post.content}</div>
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
