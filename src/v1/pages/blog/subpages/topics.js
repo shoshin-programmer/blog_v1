a9d22dee9ad76524c9705136d2c50accdb8a2e68
arvin <61683592+markryu14@users.noreply.github.com>
2020-07-09 18:28:31 +0800

    removed comments and adjust height for mobile on topics:

--- src/v1/pages/blog/subpages/topics.js
+++ src/v1/pages/blog/subpages/topics.js
@@ -7,28 +7,7 @@ export default class Topics extends React.Component {
   constructor() {
     super();
     this.state = {
-      tags: [
-        // {
-        //   topic: "algorithms",
-        //   img:
-        //     "https://images.unsplash.com/photo-1583115260445-f95fe37202ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-        // },
-        // {
-        //   topic: "journey",
-        //   img:
-        //     "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=80"
-        // },
-        // {
-        //   topic: "advice",
-        //   img:
-        //     "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=80"
-        // },
-        // {
-        //   topic: "learnings",
-        //   img:
-        //     "https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80"
-        // }
-      ]
+      tags: []
     };
   }
 
