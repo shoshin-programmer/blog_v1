5479c98f5440796ebdac7023d8fb26c0daae535c
arvin <61683592+markryu14@users.noreply.github.com>
2020-07-10 20:50:38 +0800

    about page - all views

--- src/v1/pages/homepage/about.js
+++ src/v1/pages/homepage/about.js
@@ -11,43 +11,46 @@ export default function About() {
               about
             </a>
           </div>
-          <br />
-          <div className="profile-picture-wrap">
-            <img
-              className="profile-picture"
-              alt="arvin mark atienza"
-              src={
-                "https://media-exp1.licdn.com/dms/image/C5103AQEVfJK0GiFKcA/profile-displayphoto-shrink_400_400/0?e=1599091200&v=beta&t=WxJURa3_oZyjsojM-aqJn0c7EjLCIKtwKO6LMScGlag"
-              }
-            />
+          <div className="about-inner-section">
+            <div className="profile-picture-wrap">
+              <img
+                className="profile-picture"
+                alt="arvin mark atienza"
+                src={
+                  "https://media-exp1.licdn.com/dms/image/C5103AQEVfJK0GiFKcA/profile-displayphoto-shrink_400_400/0?e=1599091200&v=beta&t=WxJURa3_oZyjsojM-aqJn0c7EjLCIKtwKO6LMScGlag"
+                }
+              />
+            </div>
+            <div className="description-wrap">
+              <p className="about-desc">
+                Hi! I am{" "}
+                <span style={{ color: "#fff" }}>Arvin Mark Atienza</span>{" "}
+                currently based on BGC Taguig City Philippines.
+              </p>
+              <p className="about-desc">
+                My goal is to provide{" "}
+                <span style={{ color: "#fff" }}>
+                  efficient, beautiful and easy-to-use websites
+                </span>{" "}
+                made for everyone that they would love to use.
+                <br />( I also dabble in{" "}
+                <span style={{ color: "#fff" }}>
+                  Data Science, Blog Websites, and other things Web.{" "}
+                </span>
+                )
+              </p>
+              <p className="about-desc">
+                Stack I used to make this website along with the blog:
+              </p>
+              <ul className="stack-list">
+                <li>HTML & CSS</li>
+                <li>React</li>
+                <li>Python</li>
+                <li>Django</li>
+                <li>JavaScript (ES6+)</li>
+              </ul>
+            </div>
           </div>
-          <br />
-          <p className="about-desc">
-            Hi! I am <span style={{ color: "#fff" }}>Arvin Mark Atienza</span>{" "}
-            currently based on BGC Taguig City Philippines.
-          </p>
-          <p className="about-desc">
-            My goal is to provide{" "}
-            <span style={{ color: "#fff" }}>
-              efficient, beautiful and easy-to-use websites
-            </span>{" "}
-            made for everyone that they would love to use.
-            <br />( I also dabble in{" "}
-            <span style={{ color: "#fff" }}>
-              Data Science, Blog Websites, and other things Web.{" "}
-            </span>
-            )
-          </p>
-          <p className="about-desc">
-            Stack I used to make this website along with the blog:
-          </p>
-          <ul>
-            <li>HTML & CSS</li>
-            <li>React</li>
-            <li>Python</li>
-            <li>Django</li>
-            <li>JavaScript (ES6+)</li>
-          </ul>
         </div>
       </div>
     </React.Fragment>
